3f9d8240e16ab9163312f524bf9586a42d603726
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-04-06 00:37:34 +0300

    Portfolio Update

---
+++ FinanceDashboard/main.py
@@ -0,0 +1,15 @@
+def ask_out():
+    print("Aterere my love 😊")
+    input("How do you do? ")
+    print("I am fine aswell its just nakumiithi sana.Although I wanted to ask...")
+    response = input("Nigwendete? (yes/no): ")
+    if response.lower() == "yes":
+        print("Yay! Nigwendete muno! 🎉")
+    elif response.lower() == "no":
+        print("Fuck you!!! Mschweew")
+    else:
+        print("Dinywa! 💛")
+
+if __name__ == "__main__":
+    """Run this my love"""
+ask_out()
